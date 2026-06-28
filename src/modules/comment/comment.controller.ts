0b5b9de1ffe65a3cb2fce4e09150f0e6d5e530cd
adnan-b-akhlas-dev <adnan.b.akhlas.dev@gmail.com>
2026-06-28 22:06:06 +0600

    feat: add get comments, get single comment and delete comment api endpoint

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,11 +1,46 @@
 import { Request, Response } from "express";
+import httpStatus from "http-status";
 import { asyncHandler } from "../../utils/asyncHandler";
+import { sendResponse } from "../../utils/sendResponse";
+import { commentService } from "./comment.service";
+import { User } from "../user/user.interface";
+import { Role } from "../../prisma/generated/prisma/enums";
 
 const getCommentsByAuthor = asyncHandler(
-  async (req: Request, res: Response): Promise<void> => {},
+  async (req: Request, res: Response): Promise<void> => {
+    const { authorId } = req.params;
+    if (!authorId) {
+      throw new Error("Author ID required on params.");
+    }
+    const data = await commentService.getCommentsByAuthorFromDb(
+      authorId as string,
+    );
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Author's comments retrieved successfully.",
+      data,
+    });
+  },
 );
 const getSingleComment = asyncHandler(
-  async (req: Request, res: Response): Promise<void> => {},
+  async (req: Request, res: Response): Promise<void> => {
+    const { commentId } = req.params;
+    if (!commentId) {
+      throw new Error("Comment ID required on params");
+    }
+    const data = await commentService.getSingleCommentFromDb(
+      commentId as string,
+    );
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Comment retrieved successfully.",
+      data,
+    });
+  },
 );
 const createComment = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {},
@@ -14,7 +49,19 @@ const updateComment = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {},
 );
 const deleteComment = asyncHandler(
-  async (req: Request, res: Response): Promise<void> => {},
+  async (req: Request, res: Response): Promise<void> => {
+    const commentId = req.params.commentId as string;
+    const authorId = (req.user as User).id;
+    const isAdmin = (req.user as User).role === Role.ADMIN;
+
+    await commentService.deleteCommentFromDb(commentId, authorId, isAdmin);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Comment removed successfully.",
+    });
+  },
 );
 const moderateComment = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {},
--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,8 +1,47 @@
-const getCommentsByAuthorFromDb = async (): Promise<void> => {};
-const getSingleCommentFromDb = async (): Promise<void> => {};
+import { prisma } from "../../lib/prisma";
+import { CommentModel } from "../../prisma/generated/prisma/models";
+
+const getCommentsByAuthorFromDb = async (
+  userId: string,
+): Promise<CommentModel[]> => {
+  const comments = await prisma.comment.findMany({
+    where: { authorId: userId },
+    orderBy: { createdAt: "desc" },
+  });
+
+  return comments;
+};
+
+const getSingleCommentFromDb = async (
+  commentId: string,
+): Promise<CommentModel> => {
+  const comment = await prisma.comment.findUniqueOrThrow({
+    where: { id: commentId },
+  });
+
+  return comment;
+};
+
 const createCommentIntoDb = async (): Promise<void> => {};
 const updateCommentIntoDb = async (): Promise<void> => {};
-const deleteCommentFromDb = async (): Promise<void> => {};
+
+const deleteCommentFromDb = async (
+  commentId: string,
+  authorId: string,
+  isAdmin: boolean,
+): Promise<void> => {
+  const comment = await prisma.comment.delete({
+    where: { id: commentId },
+  });
+
+  if (!isAdmin && authorId !== comment.authorId) {
+    throw new Error("You are not authorized to delete others comments.");
+  }
+
+  await prisma.comment.delete({
+    where: { id: commentId },
+  });
+};
 const moderateCommentIntoDb = async (): Promise<void> => {};
 
 export const commentService = {
